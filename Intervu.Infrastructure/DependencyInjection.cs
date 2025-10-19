ad7fabe25e18f70dc905aefb28df96ab0bb7ee6d
Nhi Dang <nhidy.it@gmail.com>
2025-10-19 22:11:43 +0700

    configure firebase connection and storage

--- Intervu.Infrastructure/DependencyInjection.cs
+++ Intervu.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Intervu.Infrastructure.ExternalServices;
+using Google.Apis.Auth.OAuth2;
+using FirebaseAdmin;
+using Google.Cloud.Storage.V1;
+using Intervu.Infrastructure.ExternalServices.FirebaseStorageService;
 
 namespace Intervu.Infrastructure
 {
@@ -11,8 +15,8 @@ namespace Intervu.Infrastructure
     {
         public static IServiceCollection AddPersistenceSqlServer(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContextPool<IntervuDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("SqlDefaultConnection")));
+            //services.AddDbContextPool<IntervuDbContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("SqlDefaultConnection")));
 
             // Register your repositories here
             //services.AddScoped<ISomeRepository, SomeRepository>();
@@ -22,6 +26,25 @@ namespace Intervu.Infrastructure
 
         public static IServiceCollection AddInfrastructureExternalServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var firebaseSection = configuration.GetSection("Firebase");
+            var bucketName = firebaseSection["StorageBucket"];
+            var credentialPath = firebaseSection["CredentialPath"];
+
+            if (string.IsNullOrEmpty(credentialPath))
+                throw new Exception("Firebase CredentialJson is missing in secrets.json");
+
+            var credential = GoogleCredential.FromJson(credentialPath);
+
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = credential
+                });
+            }
+
+            services.AddSingleton(StorageClient.Create(credential));
+            services.AddSingleton(bucketName);
             services.AddTransient<IMailService, EmailService>();
             services.AddTransient<IFileService, FirebaseStorageService>();
 
---
+++ Intervu.Infrastructure/ExternalServices/FirebaseStorageService/FirebaseConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervu.Infrastructure.ExternalServices.FirebaseStorageService
+{
+    public class FirebaseConfig
+    {
+        public string StorageBucket { get; set; } = string.Empty;
+        public string CredentialPath { get; set; } = string.Empty;
+    }
+}
